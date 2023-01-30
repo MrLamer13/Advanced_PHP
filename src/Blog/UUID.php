3821cf4181874cd87b9fe05691c0472874b612b0
MrLamer <mrlamer@mail.ru>
2023-01-30 13:28:49 +0700

    Практическая работа №2

---
+++ src/Blog/UUID.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GeekBrains\LevelTwo\Blog;
+
+use GeekBrains\LevelTwo\Blog\Exceptions\InvalidArgumentException;
+
+class UUID
+{
+    public function __construct(
+        private string $uuidString
+    ) {
+        if (!uuid_is_valid($uuidString)) {
+            throw new InvalidArgumentException(
+                "Неправильный UUID: $this->uuidString"
+            );
+        }
+    }
+
+// Генерируем новый случайный UUID и получаем его в качестве объекта нашего класса
+    public static function random(): self
+    {
+        return new self(uuid_create(UUID_TYPE_RANDOM));
+    }
+    public function __toString(): string
+    {
+        return $this->uuidString;
+    }
+}
